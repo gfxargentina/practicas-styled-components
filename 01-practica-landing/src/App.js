ce63355e2e7e1c3fabd1024230179ef76b3173ef
Luis Chilo <gfxargentina@gmail.com>
2022-01-14 20:15:01 -0300

    fundamentals concept about how to use styled components

--- 01-practica-landing/src/App.js
+++ 01-practica-landing/src/App.js
@@ -1,8 +1,27 @@
+import { ThemeProvider } from "styled-components";
+import { Header } from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1>Main</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <>
+        <Header />
+        <Container>
+          <h1>Main</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ 01-practica-landing/src/components/Header.js
@@ -0,0 +1,10 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+export const Header = () => {
+  return (
+    //<StyledHeader bg='red'>
+    <StyledHeader>
+      <h1>Header</h1>
+    </StyledHeader>
+  );
+};
